f21a4351273aeb6c363118e03902974a55cbb88b
Myo <132342922+WaiKyawMyo@users.noreply.github.com>
2024-06-26 17:01:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years= document.getElementById("all_years").value;
-    var interest= principal* years * rate /100;
-    var amount = parseInt(principal) + parseFloat(interest);
-var result = document.getElementById("result");
-var year = new Date().getFullYear() + parseInt(years);
-if (principal <= 0) {
-    alert("Please enter a positive number!");
-    document.getElementById("principal").focus();
+// Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
 
-}else {
-    result.innerHTML = "If you deposit $" + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of $" + amount + ",\<br\>in the year " + year + "\<br\>";
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","recommendation");
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("all_recommendations").appendChild(element); 
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+    showPopup(true);
+  }
 }
 
-}
-function updateRate()
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
 }
 
